cb6937baf09057b7c5091d85a267a17c28bcf348
MARTA Army <qbdam@yahoo.com>
2019-01-16 01:15:05 -0500

    Jan2019 update

--- stopinfo.php
+++ stopinfo.php
@@ -11,7 +11,6 @@ $nowHighThres = 1;
 $sid = $_REQUEST['sid'];
 $shortStopId = explode("_", $sid)[1];
 
-
 $debugging = isset($_REQUEST['testhour']);
 if ($debugging) {
 	$date_as_int = $_REQUEST['testhour'];
@@ -74,6 +73,9 @@ if ($debugging) {
 else {
     $data = getJson("$nextDeparturesUrl?stopid=$shortStopId");
 }
+if (isset($data['stop_name'])) $stopName = $data['stop_name'];
+else $stopName = "Undefined stop";
+
 
 ?>
 <!DOCTYPE html>
@@ -89,7 +91,7 @@ else {
 <div class="wrap">
     <header>
         <a href="http://www.martaarmy.org/" target="_blank" ></a>
-        <div class="stopname"><?=$data['stop_name']?> (<?=$shortStopId?>)</div>
+        <div class="stopname"><?=$stopName?> (<?=$shortStopId?>)</div>
     </header>
     <?php include($greetingBanner); ?>
 
@@ -98,44 +100,46 @@ else {
             <th class="route">Bus</th><th class="time">Sched.</th><th>To</th><th>Status</th>
         </tr>
 
-        <?php foreach ($data['departures'] as $dp) {
-            $mins = $dp['wait'];
-            $adh = $dp['adherence'];
-            if ($mins >= -1 || $adh > 1 && ($mins + $adh) >= -1) {
-                $hhmm = formatTime($dp['time']);
-                $dest = formatDestination($dp['destination']);
-                $status = formatStatus($adh);
+        <?php
+        if (isset($data['departures'])) {
+            foreach ($data['departures'] as $dp) {
+                $mins = $dp['wait'];
+                $adh = $dp['adherence'];
+                $tripid = $dp['trip_id'];
+                if ($mins >= -1 || $adh > 1 && ($mins + $adh) >= -1) {
+                    $hhmm = formatTime($dp['time']);
+                    $dest = formatDestination($dp['destination']);
+                    $status = formatStatus($adh);
 
-                if ($mins <= $nowHighThres && $mins >= $nowLowThres) $mins = 'Now'; // Imminent.
-                else if ($mins > $minutesThres && strcmp($status, "") != 0 && $mins + $adh > $minutesThres) { // Too soon to tell.
-                    $mins = '';
-                    $status = 'On its way';
-                }
-                else $mins .= '<span class="small">m</span>';
-                //$mins = abs($mins) . 'm' . ($mins < 0 ? ' ago' : '');    
+                    if ($mins <= $nowHighThres && $mins >= $nowLowThres) $mins = 'Now'; // Imminent.
+                    else if ($mins > $minutesThres && strcmp($status, "") != 0 && $mins + $adh > $minutesThres) { // Too soon to tell.
+                        $mins = '';
+                        $status = 'On its way';
+                    }
+                    else $mins .= '<span class="small">m</span>';
+                    //$mins = abs($mins) . 'm' . ($mins < 0 ? ' ago' : '');    
 
-                if (strcmp($status, "") != 0) {
-                    $statusCell = "<td class='status $status '><span class='mins'>$mins</span><div class='remarks'>$status</div></td>";
-                }
-                else if ($mins <= $minutesThres) {
-                    $statusCell = "<td class='status no'><div class='remarks'>No GPS</div></td>";
-                }                
-                else {
-                    $statusCell = "<td></td>";
-                }                
+                    if (strcmp($status, "") != 0) {
+                        $statusCell = "<td class='status $status '><span class='mins'>$mins</span><div class='remarks'>$status</div></td>";
+                    }
+                    else if ($mins <= $minutesThres) {
+                        $statusCell = "<td class='status no'><div class='remarks'>No GPS</div></td>";
+                    }                
+                    else {
+                        $statusCell = "<td></td>";
+                    }                
     
 echo <<<END
-    
-        <tr>
+        <tr><!-- $tripid -->
             <td class="route">$dp[route]</td>
             <td class="time">$hhmm</td>
             <td class="dest">$dest</td>
             $statusCell
         </tr>
-    
+
 END;
-    
-            }
+                } // foreach
+            } // if isset
         }?>
     </table>
     <footer>All times are approximate and may change without notice.<br/>
@@ -145,7 +149,7 @@ END;
     <script>
         var recentStops = localStorage.getItem("recentStops");
         if (recentStops == undefined) recentStops = "";
-        var stopStr = "<?=$shortStopId?>: <?=$data['stop_name']?>";
+        var stopStr = "<?=$shortStopId?>: <?=$stopName?>";
         if (recentStops.indexOf(stopStr) == -1) {
             localStorage.setItem("recentStops", stopStr + "|" + recentStops);
         }
