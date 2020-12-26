5cfc8e75f840eb8485113b69881c949eb510a776
QYAN24 <binh.damq@gmail.com>
2020-12-26 23:35:05 +0000

    Adjust streetcar stop coords

--- js/map-presets.js
+++ js/map-presets.js
@@ -52,17 +52,17 @@ var presets = {
     ],
     tram: [
         {id:"MARTA_211749",name:"KING HISTORIC DISTRICT SC",lat:33.755506,lon:-84.374961},
-        {id:"MARTA_211750",name:"DOBBS PLAZA SC",lat:33.755565,lon:-84.378785},
-        {id:"MARTA_211751",name:"AUBURN AVE @ PIEDMONT AVE SC",lat:33.755522,lon:-84.382019},
+        {id:"MARTA_211750",name:"DOBBS PLAZA SC",lat: 33.755536,lon:-84.378786},
+        {id:"MARTA_211751",name:"AUBURN AVE @ PIEDMONT AVE SC",lat:33.755556,lon:-84.382020},
         {id:"MARTA_211752",name:"WOODRUFF PARK SC",lat:33.755995,lon:-84.388148},
-        {id:"MARTA_211753",name:"PEACHTREE CENTER STATION SC",lat:33.758064,lon:-84.387532},
-        {id:"MARTA_211754",name:"CARNEGIE WAY @ TED TURNER DR SC",lat:33.759485,lon:-84.389676},
-        {id:"MARTA_211755",name:"CENTENNIAL OLYMPIC PARK SC",lat:33.759471,lon:-84.391999},
-        {id:"MARTA_211756",name:"LUCKIE ST @ CONE ST SC",lat:33.758008,lon:-84.390326},
-        {id:"MARTA_211757",name:"PARK PLACE SC",lat:33.755089,lon:-84.388238},
-        {id:"MARTA_211758",name:"HURT PARK SC",lat:33.754483,lon:-84.385283},
-        {id:"MARTA_211759",name:"SWEET AUBURN MARKET SC",lat:33.754407,lon:-84.379845},
-        {id:"MARTA_211760",name:"EGEWOOD AVE @ HILLIARD ST SC",lat:33.754408,lon:-84.376378}
+        {id:"MARTA_211753",name:"PEACHTREE CENTER STATION SC",lat:33.758068,lon:-84.387582},
+        {id:"MARTA_211754",name:"CARNEGIE WAY @ TED TURNER DR SC",lat:33.759506,lon:-84.389635},
+        {id:"MARTA_211755",name:"CENTENNIAL OLYMPIC PARK SC",lat:33.759426,lon:-84.392033},
+        {id:"MARTA_211756",name:"LUCKIE ST @ CONE ST SC",lat:33.758048,lon:-84.390322},
+        {id:"MARTA_211757",name:"PARK PLACE SC",lat:33.755091,lon:-84.388256},
+        {id:"MARTA_211758",name:"HURT PARK SC",lat:33.754462,lon:-84.385337},
+        {id:"MARTA_211759",name:"SWEET AUBURN MARKET SC",lat:33.754416,lon:-84.379993},
+        {id:"MARTA_211760",name:"EGEWOOD AVE @ HILLIARD ST SC",lat:33.754377,lon:-84.376304}
     ],
     busHub: [
         {id:"MARTA_900079",name:"CUMBERLAND MALL",lat:33.878130,lon:-84.469190},
@@ -355,8 +355,8 @@ var presets = {
 [-84.423319, 33.721664],
 [-84.423791, 33.720683],
 [-84.425030, 33.717300], // Oakland City
-[-84.427046, 33.711114]
-[-84.427367, 33.709883]
+[-84.427046, 33.711114],
+[-84.427367, 33.709883],
 [-84.428768, 33.700880], // Lakewood
 [-84.429535, 33.696119],
 [-84.429899, 33.695102],
