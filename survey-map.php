7bc9d9880e3dc63dfe578356c35a444b8fa8d833
QYAN24 <binh.damq@gmail.com>
2020-12-31 05:54:21 +0000

    Fix deadline...

--- survey-map.php
+++ survey-map.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set("America/New_York");
 
-$SURVEY_DEADLINE = strtotime("2020-01-01");
+$SURVEY_DEADLINE = strtotime("2021-01-01");
 if (strtotime("now") < $SURVEY_DEADLINE) {
     header("Location: http://" . $_SERVER["HTTP_HOST"] . "/survey-map-open.php", true, 307);
     exit;
