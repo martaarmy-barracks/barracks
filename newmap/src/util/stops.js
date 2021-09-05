0ff904c7ec71562f18b14bb34e93ba8aa1b8a513
William Casey <williamcaseylucas@gmail.com>
2021-09-05 17:21:18 -0400

    Co-authored-by: binh-dam-ibigroup <binh-dam-ibigroup@users.noreply.github.com>

--- newmap/src/util/stops.js
+++ newmap/src/util/stops.js
@@ -26,10 +26,10 @@ export function getModeClass(route) {
   }
 }
 
-export function getLetterGrade (score) {
-	if (score >= 90) return 'A'
-	else if (score >= 80) return 'B'
-	else if (score >= 70) return 'C'
-	else if (score >= 60) return 'D'
-	else return 'F'
+export function getLetterGrade(score) {
+  if (score >= 90) return "A";
+  else if (score >= 80) return "B";
+  else if (score >= 70) return "C";
+  else if (score >= 60) return "D";
+  else return "F";
 }
