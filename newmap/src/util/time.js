6125a432331b244456fdf6579cb1d350c49a17d7
QYAN24 <binh.damq@gmail.com>
2022-02-07 04:12:03 +0000

    Fix time rendering.

--- newmap/src/util/time.js
+++ newmap/src/util/time.js
@@ -3,13 +3,14 @@ export function formatTime(timeStr) {
   let hour = timeSplit[0]; // can be: "04", "12", "25".
   let ampm = "a";
   if (hour > 24) hour -= 24;
-  else if (hour === 12) {
+  if (hour === 12) {
     ampm = "p";
   } else if (hour > 12) {
     hour -= 12;
     ampm = hour === 12 ? "a" : "p"; // 12 is for 24:00 (12 am).
-  } else if (hour < 10) {
-    hour = hour + 0; // convert to number to remove leading zeros.
+  }
+  if (hour < 10) {
+    hour = +hour; // convert to number to remove leading zeros.
   }
   return hour + ":" + timeSplit[1] + ampm;
 }
